72ea8c0fd33a48216694d087621a82a1e3f59455
Jon Malmaud <malmaud@gmail.com>
2016-08-01 17:26:04 -0400

    tweak build

--- deps/build.jl
+++ deps/build.jl
@@ -1,23 +1,25 @@
 import Requests
 
+base = joinpath(Pkg.dir("TensorFlow"), "deps")
+
 @static if is_apple()
     r = Requests.get("https://malmaud.github.io/files/mac/tensorflow.zip")
-    open("downloads/tensorflow.zip", "w") do file
+    open(joinpath(base, "downloads/tensorflow.zip"), "w") do file
         write(file, r.data)
     end
-    run(`unzip -o downloads/tensorflow.zip`)
-    run(`mkdir -p bazel-out/local-fastbuild/bin/tensorflow/c`)
-    mv("libtensorflow.so", "bazel-out/local-fastbuild/bin/tensorflow/libtensorflow.dylib", remove_destination=true)
-    mv("libc_api.so", "bazel-out/local-fastbuild/bin/tensorflow/c/libc_api.dylib", remove_destination=true)
+    run(`unzip -o $base/downloads/tensorflow.zip`)
+    run(`mkdir -p $base/bazel-out/local-fastbuild/bin/tensorflow/c`)
+    mv("libtensorflow.so", "$base/bazel-out/local-fastbuild/bin/tensorflow/libtensorflow.dylib", remove_destination=true)
+    mv("libc_api.so", "$base/bazel-out/local-fastbuild/bin/tensorflow/c/libc_api.dylib", remove_destination=true)
 end
 
 @static if is_linux()
     r = Requests.get("https://malmaud.github.io/files/linux/tensorflow.zip")
-    open("downloads/tensorflow.zip", "w") do file
+    open(joinpath(base, "downloads/tensorflow.zip"), "w") do file
         write(file, r.data)
     end
-    run(`unzip -o downloads/tensorflow.zip`)
-    run(`mkdir -p bazel-out/local_linux-fastbuild/bin/tensorflow/c`)
-    mv("libtensorflow.so", "bazel-out/local_linux-fastbuild/bin/tensorflow/libtensorflow.so", remove_destination=true)
-    mv("libc_api.so", "bazel-out/local_linux-fastbuild/bin/tensorflow/c/libc_api.so", remove_destination=true)
+    run(`unzip -o $base/downloads/tensorflow.zip`)
+    run(`mkdir -p $base/bazel-out/local_linux-fastbuild/bin/tensorflow/c`)
+    mv("libtensorflow.so", "$base/bazel-out/local_linux-fastbuild/bin/tensorflow/libtensorflow.so", remove_destination=true)
+    mv("libc_api.so", "$base/bazel-out/local_linux-fastbuild/bin/tensorflow/c/libc_api.so", remove_destination=true)
 end
