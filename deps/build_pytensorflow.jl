54ce322da8e9d87b42638b1548013fa375d14d45
Jon Malmaud <malmaud@gmail.com>
2016-08-16 16:42:49 -0500

    .

--- deps/build_pytensorflow.jl
+++ deps/build_pytensorflow.jl
@@ -1,8 +1,11 @@
 using Requests
 
+# For Travis
+py_path = "/opt/python/2.7.10/bin"
+
 if is_apple()
     url = "https://malmaud.github.io/files/mac/tensorflow.zip"
-    run(`sudo easy_install --upgrade six`)
+    run(`sudo $py_path/easy_install --upgrade six`)
 elseif is_unix()
     url = "https://malmaud.github.io/files/linux/tensorflow.zip"
 end
@@ -18,7 +21,7 @@ end
 cd(base) do
     run(`unzip -o $base/downloads/tensorflow.zip`)
 end
-run(`sudo pip install $base/tensorflow-0.10.0rc0-py2-none-any.whl`)
+run(`sudo $py_path/pip install $base/tensorflow-0.10.0rc0-py2-none-any.whl`)
 
 run(`rm -f $base/tensorflow-0.10.0rc0-py2-none-any.whl`)
 run(`rm -f $base/libc_api.so`)
