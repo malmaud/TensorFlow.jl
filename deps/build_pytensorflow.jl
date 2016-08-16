707c9403871419c9aa3f14e5c61babf16c159484
Jon Malmaud <malmaud@gmail.com>
2016-08-16 15:03:43 -0500

    .

--- deps/build_pytensorflow.jl
+++ deps/build_pytensorflow.jl
@@ -11,7 +11,7 @@ base = dirname(@__FILE__)
 info("Downloading Python tensorflow wheel")
 r = Requests.get(url)
 info("Done downloading")
-run(`mkdir -p downloads`)
+run(`mkdir -p $base/edownloads`)
 open("$base/downloads/tensorflow.zip", "w") do file
     write(file, r.data)
 end
