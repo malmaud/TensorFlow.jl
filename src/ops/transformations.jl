2eaf2dd455a733876047bee004349b27e862841a
Jon Malmaud <malmaud@gmail.com>
2016-09-28 10:57:27 -0400

    Define tranpose operation on tensors

--- src/ops/transformations.jl
+++ src/ops/transformations.jl
@@ -429,3 +429,68 @@ end
 @not_implemented function boolean_mask(tensor, mask; name="")
 
 end
+
+
+
+"""
+`transpose(n::AbstractTensor, perm=nothing)`
+
+Transposes `a`. Permutes the dimensions according to `perm`.
+
+The returned tensor's dimension i will correspond to the input dimension
+`perm[i]`. If `perm` is not given, it is set to (n-1...0), where n is
+the rank of the input tensor. Hence by default, this operation performs a
+regular matrix transpose on 2-D input Tensors.
+
+For example:
+
+```python
+# 'x' is [[1 2 3]
+#         [4 5 6]]
+tf.transpose(x) ==> [[1 4]
+                     [2 5]
+                     [3 6]]
+
+# Equivalently
+tf.transpose(x, perm=[1, 0]) ==> [[1 4]
+                                  [2 5]
+                                  [3 6]]
+
+# 'perm' is more useful for n-dimensional tensors, for n > 2
+# 'x' is   [[[1  2  3]
+#            [4  5  6]]
+#           [[7  8  9]
+#            [10 11 12]]]
+# Take the transpose of the matrices in dimension-0
+tf.transpose(x, perm=[0, 2, 1]) ==> [[[1  4]
+                                      [2  5]
+                                      [3  6]]
+
+                                     [[7 10]
+                                      [8 11]
+                                      [9 12]]]
+```
+
+Args:
+  a: A `Tensor`.
+  perm: A permutation of the dimensions of `a`.
+  name: A name for the operation (optional).
+
+Returns:
+  A transposed `Tensor`.
+"""
+function Base.transpose(n::AbstractTensor, perm=nothing; name="transpose")
+    local desc
+    with_op_name(name) do
+        if perm === nothing
+            r = range(Tensor, 0, limit=rank(n))
+            perm = reverse(r, [true])
+        end
+        desc = NodeDescription("Transpose")
+        add_input(desc, Tensor(n))
+        add_input(desc, Tensor(perm))
+    end
+    Tensor(Operation(desc))
+end
+
+Base.ctranspose(n::AbstractTensor) = transpose(n)
