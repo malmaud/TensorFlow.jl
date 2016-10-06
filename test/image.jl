8af8014f0abe9addacc1c4705188626662a1ca39
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-10-06 14:30:14 -0700

    Added more image functions

--- test/image.jl
+++ test/image.jl
@@ -1,4 +1,4 @@
-sess = TensorFlow.Session()
+sess = TensorFlow.Session(TensorFlow.Graph())
 
 blank_image = TensorFlow.constant(ones((5,5,5)))
 result = run(sess, TensorFlow.image.flip_up_down(blank_image))
@@ -6,3 +6,7 @@ result = run(sess, TensorFlow.image.flip_up_down(blank_image))
 
 result = run(sess, TensorFlow.image.flip_left_right(blank_image))
 @test ones(5,5,5) == result
+
+white_png = TensorFlow.constant(ones(UInt8,5,5,1))
+result = run(sess, TensorFlow.image.decode_png(TensorFlow.image.encode_png(white_png)))
+@test result == ones(5,5,1)
