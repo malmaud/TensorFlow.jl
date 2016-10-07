6e4351e8eb243cd3b59894fe4632d1109cda5b97
Mike J Innes <mike.j.innes@gmail.com>
2016-10-08 00:47:14 +0100

    test

--- test/hello.jl
+++ test/hello.jl
@@ -22,3 +22,9 @@ result = run(sess, a+b)
 tens = TensorFlow.ones((5,5))
 @test size(tens) == (5,5)
 @test length(tens) == 25
+
+let
+  x = placeholder(Float32, shape = [1,28,28,1])
+  y = nn.conv2d(x, Variable(randn(Float32, 5,5,1,32)), [1, 1, 1, 1], "VALID")
+  @test get.(get_shape(y).dims) == [1,23,23,32]
+end
