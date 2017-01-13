8a84e5e053707450d365c215a1858910598b6218
Jon Malmaud <malmaud@gmail.com>
2017-01-13 12:29:23 -0500

    Update Travis

--- deps/build_pytensorflow.jl
+++ deps/build_pytensorflow.jl
@@ -1,12 +1,12 @@
 using Requests
 
 if is_apple()
-    url = "https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.10.0rc0-py2-none-any.whl"
-    wheel_name = "tensorflow-0.10.0rc0-py2-none-any.whl"
+    url = "https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0rc0-py2-none-any.whl"
+    wheel_name = "tensorflow-0.12.0rc0-py2-none-any.whl"
     conda_url = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
 elseif is_unix()
-    wheel_name = "tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl"
-    url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.10.0rc0-cp27-none-linux_x86_64.whl"
+    wheel_name = "tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl"
+    url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl"
     conda_url = "https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh"
 end
 
