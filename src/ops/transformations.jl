fc5c0365a0f63fd4c8d062973d9b24a765515db3
Jon Malmaud <malmaud@gmail.com>
2017-02-02 19:57:49 -0500

    Add some documentation

--- src/ops/transformations.jl
+++ src/ops/transformations.jl
@@ -192,6 +192,44 @@ function pack(nodes; axis=1, name="Pack")
     Tensor(Operation(desc), 1)
 end
 
+"""
+unpack(value; num=nothing, axis=1, name="Unpack")
+
+Unpacks the given dimension of a rank-`R` tensor into rank-`(R-1)` tensors.
+
+Unpacks `num` tensors from `value` by chipping it along the `axis` dimension.
+If `num` is not specified (the default), it is inferred from `value`'s shape.
+If `value.shape[axis]` is not known, `ValueError` is raised.
+
+For example, given a tensor of shape `(A, B, C, D)`;
+
+If `axis == 0` then the i'th tensor in `output` is the slice
+  `value[i, :, :, :]` and each tensor in `output` will have shape `(B, C, D)`.
+  (Note that the dimension unpacked along is gone, unlike `split`).
+
+If `axis == 1` then the i'th tensor in `output` is the slice
+  `value[:, i, :, :]` and each tensor in `output` will have shape `(A, C, D)`.
+Etc.
+
+This is the opposite of pack.  The numpy equivalent is
+
+    tf.unpack(x, n) = list(x)
+
+Args:
+  value: A rank `R > 0` `Tensor` to be unpacked.
+  num: An `int`. The length of the dimension `axis`. Automatically inferred
+    if `None` (the default).
+  axis: An `int`. The axis to unpack along. Defaults to the first
+    dimension. Supports negative indexes.
+  name: A name for the operation (optional).
+
+Returns:
+  The list of `Tensor` objects unpacked from `value`.
+
+Raises:
+  ValueError: If `num` is unspecified and cannot be inferred.
+  ValueError: If `axis` is out of the range [-R, R).
+"""
 function unpack(value; num=nothing, axis=1, name="Unpack")
     num_split = num==nothing ? get_shape(value, axis) : num
     local desc
@@ -252,6 +290,46 @@ function expand_dims(input, dim; name="ExpandDims")
     Tensor(Operation(desc), 1)
 end
 
+"""
+squeeze(x::AbstractTensor, squeeze_dims; name="squeeze")
+
+Removes dimensions of size 1 from the shape of a tensor.
+
+Given a tensor `input`, this operation returns a tensor of the same type with
+all dimensions of size 1 removed. If you don't want to remove all size 1
+dimensions, you can remove specific size 1 dimensions by specifying
+`axis`.
+
+For example:
+
+```prettyprint
+# 't' is a tensor of shape [1, 2, 1, 3, 1, 1]
+shape(squeeze(t)) ==> [2, 3]
+```
+
+Or, to remove specific size 1 dimensions:
+
+```prettyprint
+# 't' is a tensor of shape [1, 2, 1, 3, 1, 1]
+shape(squeeze(t, [2, 4])) ==> [1, 2, 3, 1]
+```
+
+Args:
+  input: A `Tensor`. The `input` to squeeze.
+  axis: An optional list of `ints`. Defaults to `[]`.
+    If specified, only squeezes the dimensions listed. The dimension
+    index starts at 0. It is an error to squeeze a dimension that is not 1.
+  name: A name for the operation (optional).
+  squeeze_dims: Deprecated keyword argument that is now axis.
+
+Returns:
+  A `Tensor`. Has the same type as `input`.
+  Contains the same data as `input`, but has one or more dimensions of
+  size 1 removed.
+
+Raises:
+  ValueError: When both `squeeze_dims` and `axis` are specified.
+"""
 function Base.squeeze(x::AbstractTensor, squeeze_dims; name="squeeze")
     local desc
     with_op_name(name) do
