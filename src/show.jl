517639a9e694e86153bb504e7600cb5b66febe65
Jon Malmaud <malmaud@gmail.com>
2017-02-06 18:39:00 -0500

    Fix NodeDef show

--- src/show.jl
+++ src/show.jl
@@ -68,51 +68,55 @@ end
 
 function Base.show(io::IO, desc::tensorflow.NodeDef)
     # TODO: complete this
-    println(io, "name: ", desc.name)
-    println(io, "op: ", desc.op)
-    for input_name in desc.input
-        println(io, "input: ", input_name)
-    end
-    for (attr_name, attr_value) in desc.attr
-        println(io, "attr {")
-        println(io, "  key: ", attr_name)
-        println(io, "  value {")
-        print(io, "    ")
-        if has_field(attr_value, :_type)
-            println(io, "type: $(proto_type_map[attr_value._type])")
-        elseif has_field(attr_value, :s)
-            println(io, "string: $(String(attr_value.s))")
-        elseif has_field(attr_value, :i)
-            println(io, "int: $(attr_value.i)")
-        elseif has_field(attr_value, :b)
-            println(io, "bool: $(attr_value.b)")
-        elseif has_field(attr_value, :f)
-            println(io, "float: $(attr_value.f)")
-        elseif has_field(attr_value, :tensor)
-            t = attr_value.tensor
-            println(io, "dtype: $(proto_type_map[t.dtype])")
-            sep = "    "
-            print(io, sep, "shape: ")
-            println(io, [_.size for _ in t.tensor_shape.dim])
-            print(io, sep, "content: ")
-            show_tensor = k->begin
-                f = getfield(t, k)
-                if length(f) > 0
-                    println(io, f)
-                    return true
-                end
-                return false
-            end
-            for v in [:float_val, :double_val, :int_val, :int64_val, :bool_val, :half_val, :string_val, :tensor_content]
-                if show_tensor(v)
-                    break
-                end
-            end
-        elseif has_field(attr_value, :shape)
-            print(io, "shape: ")
-            println(io, [_.size for _ in attr_value.shape.dim])
+    has_field(desc, :name) && println(io, "name: ", desc.name)
+    has_field(desc, :op) && println(io, "op: ", desc.op)
+    if has_field(desc, :input)
+        for input_name in desc.input
+            println(io, "input: ", input_name)
         end
-        println(io, "  }")
-        println(io, "}")
+    end
+    if has_field(desc, :attr)
+        for (attr_name, attr_value) in desc.attr
+            println(io, "attr {")
+            println(io, "  key: ", attr_name)
+            println(io, "  value {")
+            print(io, "    ")
+            if has_field(attr_value, :_type)
+                println(io, "type: $(proto_type_map[attr_value._type])")
+            elseif has_field(attr_value, :s)
+                println(io, "string: $(String(attr_value.s))")
+            elseif has_field(attr_value, :i)
+                println(io, "int: $(attr_value.i)")
+            elseif has_field(attr_value, :b)
+                println(io, "bool: $(attr_value.b)")
+            elseif has_field(attr_value, :f)
+                println(io, "float: $(attr_value.f)")
+            elseif has_field(attr_value, :tensor)
+                t = attr_value.tensor
+                println(io, "dtype: $(proto_type_map[t.dtype])")
+                sep = "    "
+                print(io, sep, "shape: ")
+                println(io, [_.size for _ in t.tensor_shape.dim])
+                print(io, sep, "content: ")
+                show_tensor = k->begin
+                    f = getfield(t, k)
+                    if length(f) > 0
+                        println(io, f)
+                        return true
+                    end
+                    return false
+                end
+                for v in [:float_val, :double_val, :int_val, :int64_val, :bool_val, :half_val, :string_val, :tensor_content]
+                    if show_tensor(v)
+                        break
+                    end
+                end
+            elseif has_field(attr_value, :shape)
+                print(io, "shape: ")
+                println(io, [_.size for _ in attr_value.shape.dim])
+            end
+            println(io, "  }")
+            println(io, "}")
+        end
     end
 end
