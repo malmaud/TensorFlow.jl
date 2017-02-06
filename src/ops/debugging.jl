797489f6cc5f27780aabc18d93a828821077e61e
Jon Malmaud <malmaud@gmail.com>
2017-02-06 14:15:59 -0500

    Add Print op

--- src/ops/debugging.jl
+++ src/ops/debugging.jl
@@ -11,3 +11,43 @@ for (func, name) in [
         Tensor(Operation(desc), 1)
     end
 end
+
+"""
+    Print(input, data; message=nothing, first_n=nothing, summarize=nothing, name=nothing)
+
+Prints a list of tensors.
+
+This is an identity op with the side effect of printing `data` when
+evaluating.
+
+Args:
+  input_: A tensor passed through this op.
+  data: A list of tensors to print out when op is evaluated.
+  message: A string, prefix of the error message.
+  first_n: Only log `first_n` number of times. Negative numbers log always;
+           this is the default.
+  summarize: Only print this many entries of each tensor. If None, then a
+             maximum of 3 elements are printed per input tensor.
+  name: A name for the operation (optional).
+
+Returns:
+  Same tensor as `input_`.
+"""
+function Print(input, data; message=nothing, first_n=nothing, summarize=nothing, name="Print")
+    local desc
+    with_op_name(name) do
+        desc = NodeDescription("Print")
+        add_input(desc, input)
+        add_input(desc, data)
+        if message !== nothing
+            desc["message"] = message
+        end
+        if first_n !== nothing
+            desc["first_n"] = Int64(first_n)
+        end
+        if summarize !== nothing
+            desc["summarize"] = Int64(summarize)
+        end
+    end
+    Tensor(Operation(desc), 1)
+end
--- test/debug.jl
+++ test/debug.jl
@@ -9,3 +9,11 @@ a = TensorFlow.constant([Inf])
 @test run(sess, TensorFlow.is_inf(a))[1]
 a = TensorFlow.constant([1.])
 @test run(sess, TensorFlow.is_finite(a))[1]
+
+let
+    x = constant(0)
+    y = Print(x, [x], message="test", first_n=3, summarize=5)
+    # Not sure how to test TensorFlow logging output;
+    # just make sure it doesn't run without errors for now
+    run(sess, y)
+end
