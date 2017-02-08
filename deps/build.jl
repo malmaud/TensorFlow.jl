ce40fa021d1318aad14fb3401ed7b23fc2c93dd5
Jon Malmaud <malmaud@gmail.com>
2017-02-08 18:52:38 -0500

    Bump TensorFlow version

--- deps/build.jl
+++ deps/build.jl
@@ -24,8 +24,10 @@ function try_unzip()
     end
 end
 
+const cur_version = "1.0.0-rc1"
+
 @static if is_apple()
-    r = Requests.get("https://storage.googleapis.com/malmaud-stuff/tensorflow_mac_v4.zip")
+    r = Requests.get("https://storage.googleapis.com/malmaud-stuff/tensorflow_mac_$cur_version.zip")
     open(joinpath(base, "downloads/tensorflow.zip"), "w") do file
         write(file, r.data)
     end
@@ -36,10 +38,10 @@ end
 @static if is_linux()
     if "TF_USE_GPU" ∈ keys(ENV) && ENV["TF_USE_GPU"] == "1"
         info("Building TensorFlow.jl for use on the GPU")
-        url = "https://storage.googleapis.com/malmaud-stuff/tensorflow_linux_v5.zip"
+        url = "https://storage.googleapis.com/malmaud-stuff/tensorflow_linux_$cur_version.zip"
     else
         info("Building TensorFlow.jl for CPU use only. To enable the GPU, set the TF_USE_GPU environment variable to 1 and rebuild TensorFlow.jl")
-        url = "https://storage.googleapis.com/malmaud-stuff/tensorflow_linux_cpu_v4.zip"
+        url = "https://storage.googleapis.com/malmaud-stuff/tensorflow_linux_cpu_$cur_version.zip"
     end
     r = Requests.get(url)
     open(joinpath(base, "downloads/tensorflow.zip"), "w") do file
