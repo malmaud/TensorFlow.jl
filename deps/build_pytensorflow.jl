764a99ed8df984b93da5c6ef7f6ce0d38a3bd3ae
Jon Malmaud <malmaud@gmail.com>
2017-03-16 15:18:48 -0400

    Fix Travis for TensorFlow 1.0

--- deps/build_pytensorflow.jl
+++ deps/build_pytensorflow.jl
@@ -1,12 +1,8 @@
 using Requests
 
 if is_apple()
-    url = "https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-0.12.0rc0-py2-none-any.whl"
-    wheel_name = "tensorflow-0.12.0rc0-py2-none-any.whl"
     conda_url = "https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh"
 elseif is_unix()
-    wheel_name = "tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl"
-    url = "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0rc0-cp27-none-linux_x86_64.whl"
     conda_url = "https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh"
 end
 
@@ -18,18 +14,10 @@ info("Downloading miniconda")
 run(`wget $conda_url -O $base/downloads/miniconda.sh`)
 run(`/bin/bash $base/downloads/miniconda.sh -f -b -p $base/miniconda`)
 
-
-info("Downloading Python tensorflow wheel")
-r = Requests.get(url)
-info("Done downloading")
-open("$base/$wheel_name", "w") do file
-    write(file, r.data)
-end
-
 py_path = joinpath(base, "miniconda", "bin")
 run(`$py_path/conda upgrade -y setuptools`)
-run(`$py_path/pip install --upgrade --ignore-installed $base/$wheel_name`)
-run(`rm -f $base/$wheel_name`)
+run(`$py_path/pip install --upgrade --ignore-installed tensorflow`)
+
 
 ENV["PYTHON"] = "$py_path/python"
 Pkg.build("PyCall")
