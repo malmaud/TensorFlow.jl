c2298d828fba6fced2dde6a8ece40c12dd89df17
Jon Malmaud <malmaud@gmail.com>
2017-04-03 22:59:14 -0400

    Update readers

--- src/io/readers.jl
+++ src/io/readers.jl
@@ -16,30 +16,14 @@ end
 @reader TFRecordReader
 @reader FixedLengthRecordReader
 
-@op function WholeFileReader(; name="WholeFileReader")
-    local desc
-    with_op_name(name) do
-        desc = NodeDescription("WholeFileReader")
-    end
-    return WholeFileReader(Operation(desc))
+@op function WholeFileReader(; kwargs...)
+    WholeFileReader(get_op(Ops.whole_file_reader_v2(; kwargs...)))
 end
 
-@op function TextLineReader(skip_header_lines::Int=0; name="TextLineReader")
-    local desc
-    with_op_name(name) do
-        desc = NodeDescription("TextLineReader")
-        desc["skip_header_lines"] = Int64(skip_header_lines)
-    end
-    return TextLineReader(Operation(desc))
+@op function TextLineReader(; kwargs...)
+    TextLineReader(get_op(Ops.text_line_reader_v2(; kwargs...)))
 end
 
-@op function Base.read(reader::AbstractReader, queue::tf.AbstractQueue; name=nothing)
-    local desc
-    with_op_name(name, "ReaderRead") do
-        desc = NodeDescription("ReaderRead")
-        add_input(desc, Tensor(reader.op))
-        add_input(desc, Tensor(queue.op))
-    end
-    op = tf.Operation(desc)
-    tf.Tensor(op, 1), tf.Tensor(op, 2)
+@op function Base.read(reader::AbstractReader, queue::tf.AbstractQueue; kwargs...)
+    Ops.reader_read_v2(Tensor(reader.op), Tensor(queue.op); kwargs...)
 end
