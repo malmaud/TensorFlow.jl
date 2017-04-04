8ce66d227b87c4de5263067ff00757c63fc3cbb7
Jon Malmaud <malmaud@gmail.com>
2017-04-03 22:53:10 -0400

    Convert more functions

--- src/ops/math.jl
+++ src/ops/math.jl
@@ -1,116 +1,60 @@
-"""
-`function add_n(inputs; name="AddN")`
+import .Ops:
+    add_n,
+    arg_min,
+    arg_max,
+    maximum,
+    minimum,
+    add,
+    sub,
+    mat_mul,
+    mul,
+    pow,
+    matrix_solve,
+    matrix_triangular_solve,
+    matrix_solve_ls,
+    self_adjoint_eig,
+    cholesky,
+    cross,
+    neg,
+    square,
+    shape,
+    lbeta,
+    zeta,
+    polygamma,
+    matrix_inverse,
+    matrix_determinant,
+    diag,
+    matrix_diag_part,
+    unsorted_segment_sum,
+    unsorted_segment_max,
+    segment_sum,
+    segment_max,
+    segment_mean,
+    segment_min,
+    segment_prod
 
-Adds all input tensors element-wise.
-
-Args:
-  inputs: A list of `Tensor` objects, each with same shape and type.
-  name: A name for the operation (optional).
-
-Returns:
-  A `Tensor` of same shape and type as the elements of `inputs`.
-
-Raises:
-  ValueError: If `inputs` don't all have same shape and dtype or the shape
-  cannot be inferred.
-"""
-@op function add_n(inputs; name=nothing)
-    local desc
-    with_op_name(name, "AddN") do
-        desc = NodeDescription("AddN")
-        add_input(desc, Tensor.(inputs))
-    end
-    Tensor(Operation(desc))
-end
-
-"""
-Returns the index with the smallest value across dimensions of a tensor.
-
-Args:
-* `n`: A `Tensor`.
-* `dim`: A `Tensor`. Describes which dimension of `n` to reduce across.
-
-Returns:
-A `Tensor` of type `Int64`.
-"""
-@op function argmin(n::AbstractTensor, axis; name=nothing)
-    local desc
-    with_op_name(name, "ArgMin") do
-        desc = NodeDescription("ArgMin")
-        add_input(desc, Tensor(n))
-        add_input(desc, convert(Tensor{Int32}, axis))
-    end
-    Tensor(Operation(desc), 1)
-end
 
 @op Base.indmin(n::AbstractTensor, dim; name=nothing) = argmin(n, dim-1; name=name)+1
 
-"""
-Returns the index with the largest value across dimensions of a tensor.
-
-Args:
-* `n`: A `Tensor`.
-* `dim`: A `Tensor`. Describes which dimension of `n` to reduce across.
-
-Returns:
-A `Tensor` of type `Int64`.
-"""
-@op function argmax(n::AbstractTensor, axis; name=nothing)
-    local desc
-    with_op_name(name, "ArgMax") do
-        desc = NodeDescription("ArgMax")
-        add_input(desc, Tensor(n))
-        add_input(desc, convert(Tensor{Int32}, axis))
-    end
-    Tensor(Operation(desc), 1)
-end
-
 @op Base.indmax(n::AbstractTensor, dim; name=nothing) = argmax(n, dim-1; name=name)+1
 
-@op function Base.max(x::AbstractTensor, y, name=nothing)
-    local desc
-    with_op_name(name, "Maximum") do
-        desc = NodeDescription("Maximum")
-        add_input(desc, Tensor(x))
-        add_input(desc, Tensor(y))
-    end
-    Tensor(Operation(desc))
-end
-
-@op function Base.min(x::AbstractTensor, y, name=nothing)
-    local desc
-    with_op_name(name, "Minimum") do
-        desc = NodeDescription("Minimum")
-        add_input(desc, Tensor(x))
-        add_input(desc, Tensor(y))
-    end
-    Tensor(Operation(desc))
-end
+@op Base.max(x::AbstractTensor, y; kwargs...) = Ops.maximum(x, y; kwargs...)
+@op Base.min(x::AbstractTensor, y; kwargs...) = Ops.minimum(x, y; kwargs...)
 
 
-for (bin_op, jl_func_name, tf_func_name) in [
-    (:+, :add, "Add"),
-    (:-, :subtract, "Sub"),
-    (:*, :matmul, "MatMul"),
-    (:.*, :multiply, "Mul"),
-    (:/, :divide, "Div"),
-    (:^, :pow, "Pow")]
-    @eval function $jl_func_name(n1::AbstractTensor, n2::AbstractTensor; name=nothing)
-        local desc
-        with_op_name(name, lowercase($tf_func_name)) do
-            n1 = Tensor(n1)
-            n2 = Tensor(n2)
-            desc = NodeDescription($tf_func_name)
-            add_input(desc, n1)
-            add_input(desc, n2)
-        end
-        Tensor(Operation(desc), 1)
-    end
-    jl_func_name == :multiply && continue  # Defined below
+for (bin_op, jl_func_name) in [
+    (:+, :add),
+    (:-, :sub),
+    (:*, :mat_mul),
+    (:/, :div),
+    (:^, :pow)]
+
     @eval $bin_op(n1::AbstractTensor, n2::AbstractTensor) = $jl_func_name(n1, n2)
     @eval $bin_op(n1::AbstractTensor, n2) = $jl_func_name(n1, tf_promote(n1, n2))
     @eval $bin_op(n1, n2::AbstractTensor) = $jl_func_name(tf_promote(n2, n1), n2)
 end
+
+const matmul = mat_mul
 
 @static if VERSION > v"0.6-"  # Cope with changes in broadcasting in Julia 0.6
     Base.broadcast(::typeof(*), n1::AbstractTensor, n2::AbstractTensor) = multiply(n1, n2)
@@ -169,77 +113,8 @@ Returns:
     Tensor(Operation(desc), 1)
 end
 
-# TO DO provide the aliases for Base functions
-@op function matrix_solve(matrix, rhs; adjoint=false, name=nothing)
-    local desc
-    with_op_name(name, "MatrixSolve") do
-        desc = NodeDescription("MatrixSolve")
-        matrix = Tensor(matrix)
-        rhs = Tensor(rhs)
-        add_input(desc, matrix)
-        add_input(desc, rhs)
-        desc["adjoint"] = adjoint
-    end
-    Tensor(Operation(desc), 1)
-end
-
-@op function matrix_triangular_solve(matrix, rhs; lower=true, adjoint=false, name=nothing)
-    local desc
-    with_op_name(name, "MatrixTriangularSolve") do
-        desc = NodeDescription("MatrixTriangularSolve")
-        matrix = Tensor(matrix)
-        rhs = Tensor(rhs)
-        add_input(desc, matrix)
-        add_input(desc, rhs)
-        desc["lower"] = lower
-        desc["adjoint"] = adjoint
-    end
-    Tensor(Operation(desc), 1)
-end
-
-@op function matrix_solve_ls(matrix, rhs; l2regularizer=0., fast=true, name=nothing)
-    local desc
-    with_op_name(name, "MatrixSolveLS") do
-        desc = NodeDescription("MatrixSolveLS")
-        matrix = Tensor(matrix)
-        rhs = Tensor(rhs)
-        add_input(desc, matrix)
-        add_input(desc, rhs)
-        desc["l2regularizer"] = l2regularizer
-        desc["fast"] = fast
-    end
-    Tensor(Operation(desc), 1)
-end
-
-@op function self_adjoint_eig(tensor; name=nothing)
-    local desc
-    with_op_name(name, "SelfAdjointEig") do
-        desc = NodeDescription("SelfAdjointEigV2")
-        add_input(desc, Tensor(tensor))
-    end
-    op = Operation(desc)
-    [Tensor(op, 1), Tensor(op, 2)]
-end
-
-@op function cholesky(input; name=nothing)
-    local desc
-    with_op_name(name, "Cholesky") do
-        desc = NodeDescription("Cholesky")
-        add_input(desc, Tensor(input))
-    end
-    Tensor(Operation(desc), 1)
-end
-
-@op function Base.cross(n1::AbstractTensor, n2::AbstractTensor; name=nothing)
-    local desc
-    with_op_name(name, "Cross") do
-        n1 = Tensor(n1)
-        n2 = Tensor(n2)
-        desc = NodeDescription("Cross")
-        add_input(desc, n1)
-        add_input(desc, n2)
-    end
-    Tensor(Operation(desc), 1)
+@op function Base.cross(n1::AbstractTensor, n2::AbstractTensor; kwargs...)
+    Ops.cross(n1, n2; kwargs...)
 end
 
 *(x::Number, n::AbstractTensor) = x.*n    # For supporting notation like `2x`
@@ -252,117 +127,48 @@ else
     Base.broadcast(::typeof(^), n::AbstractTensor, x) = n^x
 end
 
-for (jl_func_name, tf_func_name) in [
-    (:sign, "Sign"),
-    (:negative, "Neg"),
-    (:square, "Square"),
-    (:shape, "Shape")]
-    @eval @op function $jl_func_name(n::AbstractTensor; name=nothing)
-        local desc
-        with_op_name(name, $tf_func_name) do
-            n = Tensor(n)
-            desc = NodeDescription($tf_func_name)
-            add_input(desc, n)
-        end
-        Tensor(Operation(desc), 1)
+for jl_func_name in [
+    :log,
+    :exp,
+    :ceil,
+    :floor,
+    :sqrt,
+    :abs,
+    :cos,
+    :sin,
+    :tan,
+    :atan,
+    :asin,
+    :acos,
+    :tanh,
+    :lgamma,
+    :erf,
+    :erfc,
+    :real,
+    :imag,
+    :sign,
+    :conj,
+    :round]
+    @eval @op function Base.$jl_func_name(n::AbstractTensor; kwargs...)
+        Ops.$jl_func_name(n; kwargs...)
     end
-end
-
-for (jl_func_name, tf_func_name) in [
-    (:log, "Log"),
-    (:exp, "Exp"),
-    (:ceil, "Ceil"),
-    (:floor, "Floor"),
-    (:sqrt, "Sqrt"),
-    (:abs, "Abs"),
-    (:cos, "Cos"),
-    (:sin, "Sin"),
-    (:tan, "Tan"),
-    (:atan, "Atan"),
-    (:asin, "Asin"),
-    (:acos, "Acos"),
-    (:tanh, "Tanh"),
-    (:lgamma, "Lgamma"),
-    (:erf, "Erf"),
-    (:erfc, "Erfc"),
-    (:real, "Real"),
-    (:imag, "Imag"),
-    (:conj, "Conj"),
-    (:round, "Round")
-    ]
-    @eval @op function Base.$jl_func_name(n::AbstractTensor; name=nothing)
-        local desc
-        with_op_name(name, $tf_func_name) do
-            n = Tensor(n)
-            desc = NodeDescription($tf_func_name)
-            add_input(desc, n)
-        end
-        Tensor(Operation(desc), 1)
-    end
-end
-
-function Base.round{T}(::Type{T}, value::AbstractTensor)
-    convert(Tensor{T}, round(value))
-end
-
-
-@op function Base.lbeta(x1::AbstractTensor, x2; name=nothing)
-    local out
-    with_op_name(name, "lbeta") do
-        x1 = Tensor(x1)
-        x2 = Tensor(x2)
-        out = lgamma(x1) + lgamma(x2) - lgamma(x1 + x2)
-    end
-    out
-end
-
-#two arg special functions
-for (jl_func_name, tf_func_name) in [
-    (:zeta, "Zeta"),
-    (:polygamma, "Polygamma")]
-    @eval @op function Base.$jl_func_name(x::AbstractTensor, q::AbstractTensor; name=nothing)
-        local desc
-        with_op_name(name, $tf_func_name) do
-            x = Tensor(x)
-            q = Tensor(q)
-            desc = NodeDescription($tf_func_name)
-            add_input(desc, x)
-            add_input(desc, q)
-        end
-        Tensor(Operation(desc), 1)
-    end
-    @eval $jl_func_name(x::AbstractTensor, q) = $jl_func_name(x, tf_promote(x, q))
 end
 
 -(n::AbstractTensor) = negative(n)
 
-@op function Base.complex(x_r::AbstractTensor, x_i::AbstractTensor; name=nothing)
-    local desc
-    with_op_name(name, "Complex") do
-        x_r = Tensor(x_r)
-        x_i = Tensor(x_i)
-        desc = NodeDescription("Complex")
-        add_input(desc, x_r)
-        add_input(desc, x_i)
-    end
-    Tensor(Operation(desc), 1)
+@op function Base.complex(x_r::AbstractTensor, x_i::AbstractTensor; kwargs...)
+    Ops.complex(x_r, x_i; kwargs...)
 end
 
 # Matrix math
 
 for (jl_func_name, tf_func_name) in [
-    (:inv, "MatrixInverse"),
-    (:det, "MatrixDeterminant"),
-    (:diagm, "Diag"),
-    (:diag, "MatrixDiagPart")]
-    @eval @op function Base.$jl_func_name(n::AbstractTensor; name=nothing)
-        local desc
-        with_op_name(name, $tf_func_name) do
-            n = Tensor(n)
-            desc = NodeDescription($tf_func_name)
-            add_input(desc, n)
-        end
-        Tensor(Operation(desc), 1)
+    (:inv, :matrix_inverse),
+    (:det, :matrix_determinant),
+    (:diagm, :diag),
+    (:diag, :matrix_diag_part)]
+    @eval @op function Base.$jl_func_name(n::AbstractTensor; kwargs...)
+        Ops.$tf_func_name(n; kwargs...)
     end
 end
 
@@ -401,30 +207,4 @@ for reduction in [:sum, :prod, :min, :max, :all, :any, :mean]
             Tensor(Operation(desc), 1)
         end
     end
-end
-
-for reduction in [:sum, :prod, :min, :max, :mean]
-    func_name = ucfirst(string(reduction))
-    @eval @op function $(Symbol("segment_", reduction))(n::AbstractTensor, segment_indices; name=nothing)
-        segment_indices = convert(Tensor{Int32}, segment_indices) - 1
-        local desc
-        with_op_name(name, string("Segment", $func_name)) do
-            desc = NodeDescription("Segment"*$func_name)
-            add_input(desc, Tensor(n))
-            add_input(desc, Tensor(segment_indices))
-        end
-        Tensor(Operation(desc), 1)
-    end
-end
-
-@op function unsorted_segment_sum(data, segment_ids, num_segments; name=nothing)
-    local desc
-    with_op_name(name,"UnsortedSegmentSum") do
-        desc = NodeDescription("UnsortedSegmentSum")
-        segment_ids0 = segment_ids - 1
-        add_input(desc,Tensor(data))
-        add_input(desc,Tensor(segment_ids0))
-        add_input(desc, convert(Tensor{Int32}, num_segments))
-    end
-    Tensor(Operation(desc), 1)
 end
