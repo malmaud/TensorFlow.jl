99cd7e75fd883bf4d4d8cddd2d5c4f64edbdd7e8
Jon Malmaud <malmaud@gmail.com>
2017-05-30 17:58:49 -0400

    Add tests

--- test/training.jl
+++ test/training.jl
@@ -1,3 +1,4 @@
+
 using Base.Test
 using TensorFlow
 
@@ -13,3 +14,12 @@ using TensorFlow
         @test size(run(sess, x1)) == (50, 1)
     end
 end
+
+
+sess = Session(Graph())
+x = get_variable("x", (50,10), Float64)
+x3n5 = gather(x, [3, 5])
+cost = reduce_sum(x3n5)
+optimizer = train.minimize(train.AdamOptimizer(0.1), cost)
+run(sess, global_variables_initializer())
+@test size(run(sess, x3n5)) == (2, 10)
