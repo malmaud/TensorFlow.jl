0718558fbc21a4a6ac3835a230be16d324e4eeae
Jon Malmaud <malmaud@gmail.com>
2017-05-31 16:21:06 -0400

    Re-add slice test

--- test/training.jl
+++ test/training.jl
@@ -1,4 +1,3 @@
-
 using Base.Test
 using TensorFlow
 
@@ -10,6 +9,18 @@ using TensorFlow
     optimizer = train.minimize(train.AdamOptimizer(0.1), cost)
     run(sess, global_variables_initializer())
     @test size(run(sess, x3n5)) == (2, 10)
+end
+
+@testset "slice" begin
+    let
+        sess = Session(Graph())
+        x = get_variable("x", (50,10), Float64)
+        x1 = TensorFlow.slice(x, [1,1],[-1,1])
+        cost = reduce_sum(x1)
+        optimizer = train.minimize(train.AdamOptimizer(), cost)
+        run(sess, global_variables_initializer())
+        @test size(run(sess, x1)) == (50, 1)
+    end
 end
 
 @testset "Making a functioning network: Issue #147" begin
