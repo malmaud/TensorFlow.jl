b1fbab8ab89aace0d9822344df054de35bef7297
Jon Malmaud <malmaud@gmail.com>
2017-06-08 15:59:28 -0400

    Tweak tests

--- test/io.jl
+++ test/io.jl
@@ -1,15 +1,14 @@
 using TensorFlow
 using Base.Test
 
-mktempdir() do dirname
-    filename = joinpath(dirname, "tfrecords")
-    TensorFlow.io.RecordWriter(filename) do writer
-        write(writer, UInt8[1,2])
-        write(writer, "hi")
-    end
-
-    reader = TensorFlow.io.RecordIterator(filename)
-    records = collect(reader)
-    @test records[1] == UInt8[1, 2]
-    @test records[2] == Vector{UInt8}("hi")
+filename = tempname()
+TensorFlow.io.RecordWriter(filename) do writer
+    write(writer, UInt8[1,2])
+    write(writer, "hi")
 end
+
+reader = TensorFlow.io.RecordIterator(filename)
+records = collect(reader)
+@test !isempty(records)
+@test records[1] == UInt8[1, 2]
+@test records[2] == Vector{UInt8}("hi")
