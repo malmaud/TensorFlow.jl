1547a2b799730f1628526d55d4f9acf4c35d3d4b
Jon Malmaud <malmaud@gmail.com>
2017-06-19 19:40:51 -0400

    Update documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,27 @@
 using Documenter
 using TensorFlow
 
-# makedocs(modules=[TensorFlow]) # Crashes
-makedocs()
+makedocs(modules=[TensorFlow],
+         format=:html,
+         sitename="TensorFlow.jl",
+         authors="Jon Malmaud and contributors.",
+         analytics = "UA-1123761-11",
+         pages = [
+         "Home"=>"index.md",
+         "Manual"=>
+            ["MNIST tutorial"=>"tutorial.md",
+             "Visualizing with Tensorboard"=>"visualization.md",
+             "Using queues for loading your data"=>"io.md",
+             "Shape inference"=>"shape_inference.md",
+             "Saving and restoring"=>"saving.md"],
+          "Reference"=>
+            ["Core functions"=>"core.md",
+             "Operations"=>"ops.md",
+             "IO pipelines with queues"=>"io_ref.md"],
+          "Examples"=>
+            ["Basic usage"=>"basic_usage.md",
+             "Logistic regression"=>"logistic.md"]
+        ])
+
+deploydocs(repo="github.com/malmaud/TensorFlow.jl.git",
+           julia="0.6")
