25753b292880176e51e057ce734bbe146c017941
Lyndon White <oxinabox@ucc.asn.au>
2018-05-24 17:30:10 +0800

    =Add ability to import names that are already used in Base

--- test/ops.jl
+++ test/ops.jl
@@ -8,3 +8,11 @@ using Base.Test
     as_string = import_op("AsString")
     @test run(session, as_string(2)) == "2"
 end
+
+
+
+import_op("Atan2") #Must be outside testset as per https://github.com/JuliaLang/julia/issues/27244
+@testset "Importing a name that is used by Base" begin
+    session = Session(Graph())
+    @test run(session, Ops.atan2(Tensor(1.0), Tensor(1.0))) > 0 
+end
