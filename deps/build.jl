4c43fa2194f77c35c88110bb48228a2e19020b76
JohnnyChen <johnnychen94@hotmail.com>
2018-10-31 13:48:27 +0800

    use sha256 to check the validity of file

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,8 @@
 using PyCall
 using Conda
+using SHA
 
-const cur_version = "1.10.0"
+const cur_version = "1.10.0" # change checksum while changing version
 const cur_py_version = "1.10.0"
 
 
@@ -73,34 +74,44 @@ mkpath(lib_dir)
 mkpath(bin_dir)
 
 
-function download_and_unpack(url)
-    tensorflow_zip_path = joinpath(base, "downloads/tensorflow.tar.gz")
-    try
-        run(`tar -xzf $tensorflow_zip_path -C downloads`)
-    catch
-        download(url, tensorflow_zip_path)
-        run(`tar -xzf $tensorflow_zip_path -C downloads`)
+function download_and_unpack(url, checksum)
+    tensorflow_zip_path = joinpath(download_dir, "tensorflow.tar.gz")
+
+    use_cache = false
+    isfile(tensorflow_zip_path) && open(tensorflow_zip_path) do f
+        use_cache = bytes2hex(sha256(f)) == checksum
     end
+        
+    if use_cache
+        @info "using cached $tensorflow_zip_path"
+    else
+        @info "downloading tensorflow from $url"
+        download(url, tensorflow_zip_path)
+    end
+    run(`tar -xzf $tensorflow_zip_path -C downloads`)
 end
 
 @static if Sys.isapple()
     if use_gpu
-        url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-darwin-x86_64-$cur_version.tar.gz"
-    else
-        url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-$cur_version.tar.gz"
+        @warn("No support for TF_USE_GPU on OS X - to enable the GPU, build TensorFlow from source. Falling back to CPU")
+        use_gpu = false
     end
-    download_and_unpack(url)
-    mv("$lib_dir/libtensorflow.so", "usr/bin/libtensorflow.dylib", force=true)
-    mv("$lib_dir/libtensorflow_framework.so", "usr/bin/libtensorflow_framework.so", force=true)
+    url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-$cur_version.tar.gz"
+    checksum = "a9d895ca6a974d8a946e26477339a80fb635419707562200bbcc7352cf71e086" # sha256sum
+    download_and_unpack(url, checksum)
+    mv("$lib_dir/libtensorflow.so", "$bin_dir/libtensorflow.dylib", force=true)
+    mv("$lib_dir/libtensorflow_framework.so", "$bin_dir/libtensorflow_framework.so", force=true)
 end
 
 @static if Sys.islinux()
     if use_gpu
         url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-$cur_version.tar.gz"
+        checksum = "96074aa88aa74fd0a70a864166e218a2471b00a0504b9f5c963dfadb99d6c66c" # sha256sum
     else
         url = "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-$cur_version.tar.gz"
+        checksum = "abf3baa49d460a2f087bc034d5e65f765817f6d9eede564fd848fef616bb4b87" # sha256sum
     end
-    download_and_unpack(url)
-    mv("$lib_dir/libtensorflow.so", "usr/bin/libtensorflow.so", force=true)
-    mv("$lib_dir/libtensorflow_framework.so", "usr/bin/libtensorflow_framework.so", force=true)
+    download_and_unpack(url, checksum)
+    mv("$lib_dir/libtensorflow.so", "$bin_dir/libtensorflow.so", force=true)
+    mv("$lib_dir/libtensorflow_framework.so", "$bin_dir/libtensorflow_framework.so", force=true)
 end
