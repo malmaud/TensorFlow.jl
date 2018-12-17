fbcc0da874414118c10d489e5b148ef92ae98e9f
kailaix <kailaix@stanford.edu>
2018-12-17 00:41:02 -0800

    update version

--- test/train.jl
+++ test/train.jl
@@ -78,3 +78,75 @@ end
         end
     end
 end
+
+
+@test "optimizers" begin
+    using Distributions
+    # Generate some synthetic data
+    x = randn(100, 50)
+    w = randn(50, 10)
+    y_prob = exp.(x*w)
+    y_prob ./= sum(y_prob,dims=2)
+
+    function draw(probs)
+        y = zeros(size(probs))
+        for i in 1:size(probs, 1)
+            idx = rand(Categorical(probs[i, :]))
+            y[i, idx] = 1
+        end
+        return y
+    end
+
+    y = draw(y_prob)
+
+    # Build the model
+    sess = Session(Graph())
+
+    X = placeholder(Float64, shape=[-1, 50])
+    Y_obs = placeholder(Float64, shape=[-1, 10])
+
+    variable_scope("logisitic_model"; initializer=Normal(0, .001)) do
+        global W = get_variable("W", [50, 10], Float64)
+        global B = get_variable("B", [10], Float64)
+    end
+
+    Y=nn.softmax(X*W + B)
+
+    
+
+
+    Loss = -reduce_sum(log(Y).*Y_obs)
+    
+    ### NadamOptimizer
+    optimizer = train.NadamOptimizer()
+    minimize_op = train.minimize(optimizer, Loss)
+    # Run training
+    run(sess, global_variables_initializer())
+    for epoch in 1:100
+        cur_loss, _ = run(sess, [Loss, minimize_op], Dict(X=>x, Y_obs=>y))
+        println(@sprintf("[NadamOptimizer]Current loss is %.2f.", cur_loss))
+    end
+
+    ### AMSGradOptimizer
+    optimizer = train.AMSGradOptimizer()
+    minimize_op = train.minimize(optimizer, Loss)
+    # Run training
+    run(sess, global_variables_initializer())
+    for epoch in 1:100
+        cur_loss, _ = run(sess, [Loss, minimize_op], Dict(X=>x, Y_obs=>y))
+        println(@sprintf("[AMSGradOptimizer]Current loss is %.2f.", cur_loss))
+    end
+
+    function mycallback(handle)
+        res = run(sess, Loss, Dict(X=>x, Y_obs=>y))
+        println("[$m]iter \$(handle.iteration): \$(res)")
+        return false # so it do not stop 
+    end
+
+    for m in ["AGD", "CG", "BFGS", "LBFGS"]
+        run(sess, global_variables_initializer())
+        options = Optim.Options(show_trace = false, iterations=1000, callback = mycallback, allow_f_increases=true)
+        OptimMinimize(sess, Loss, feed_dict = Dict(X=>x, Y_obs=>y), options=options, method=m)
+    end
+
+end
