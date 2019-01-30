c5926a762b7ce01783b0e51217173847d867fd32
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-01-30 11:17:39 +0000

    More  detailed comments

--- src/core.jl
+++ src/core.jl
@@ -1795,6 +1795,9 @@ end
 function get_tensor_num_dims(tensor::AbstractTensor)
     if tensor.op.op_name == "NoOp"
         # Work  around https://github.com/malmaud/TensorFlow.jl/issues/466
+        # In short libtensorflow segfaults when you ask for the shape
+        # of a `no_op`. Which includes most control flow statements like `group`
+        # which are used e.g. in `minimize`
         return 0
     end
     
