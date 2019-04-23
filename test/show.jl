37a16f4b1305590e0dbf5247d040176c33b41f3c
Blair Jennings <blair0011@hotmail.com>
2019-04-22 20:36:22 -0700

    Adding message bout tensorboard

--- test/show.jl
+++ test/show.jl
@@ -2,5 +2,9 @@ using TensorFlow
 using Test
 
 @testset "Tensorboard" begin
-    TensorFlow.get_tensorboard()
+    if Sys.iswindows()
+        println("Use the python Tensorboard. Setting up Julia isn't worth the effort since you need a working python 3.x anyways.")       
+    else
+        TensorFlow.get_tensorboard()
+    end
 end
